db2d545adf21a89f83b5891e5fdcc9ec68ca46c4
KeruFC <kerubela1@gmail.com>
2020-10-19 14:21:56 +0200

    fragment_add_shopping

--- app/src/main/java/com/example/madlevel4task1/ShoppingFragment.kt
+++ app/src/main/java/com/example/madlevel4task1/ShoppingFragment.kt
@@ -1,15 +1,34 @@
 package com.example.madlevel4task1
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_shopping.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class ShoppingFragment : Fragment() {
+
+
+    private val shoppingItems = arrayListOf<Shoppingitem>()
+    private val shoppingitemAdapter = ShoppingitemAdapter(shoppingItems)
+    private lateinit var shoppingitemRepository: ShoppingitemRepository
+    private val mainScope = CoroutineScope(Dispatchers.Main)
+
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -21,5 +40,97 @@ class ShoppingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        shoppingitemRepository = ShoppingitemRepository(requireContext())
+        getShoppingListFromDatabase()
+
+        initRv()
+
+        fab_add.setOnClickListener{
+            showAddProductdialog()
+        }
     }
+
+    private fun initRv() {
+        rv_shopping_list.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        rv_shopping_list.adapter = shoppingitemAdapter
+        rv_shopping_list.setHasFixedSize(true)
+        rv_shopping_list.addItemDecoration(
+            DividerItemDecoration(
+                activity,
+                DividerItemDecoration.VERTICAL
+            )
+        )
+
+        createItemTouchHelper().attachToRecyclerView(rv_shopping_list)
+    }
+
+    @SuppressLint("InflateParams")
+    private fun showAddProductdialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle(getString(R.string.add_product_dialog_title))
+        val dialogLayout = layoutInflater.inflate(R.layout.add_product_dialog, null)
+        val productName = dialogLayout.findViewById<EditText>(R.id.txt_product_name)
+        val amount = dialogLayout.findViewById<EditText>(R.id.txt_amount)
+
+        builder.setView(dialogLayout)
+        builder.setPositiveButton(R.string.dialog_ok_btn) { _: DialogInterface, _: Int ->
+            addProduct(productName, amount)
+        }
+        builder.show()
+    }
+
+    private fun addProduct(txtProductName: EditText, txtAmount: EditText) {
+        if (validateFields(txtProductName, txtAmount)) {
+            mainScope.launch {
+                val product = Product(
+                    productName = txtProductName.text.toString(),
+                    productQuantity = txtAmount.text.toString().toShort()
+                )
+
+                withContext(Dispatchers.IO) {
+                    productRepository.insertProduct(product)
+                }
+
+                getShoppingListFromDatabase()
+            }
+        }
+    }
+
+    private fun getShoppingListFromDatabase() {
+        mainScope.launch {
+            val shoppingList = withContext(Dispatchers.IO) {
+                shoppingitemRepository.getAllProducts()
+            }
+            this@ShoppingFragment.shoppingItems.clear()
+            this@ShoppingFragment.shoppingItems.addAll(shoppingList)
+            this@ShoppingFragment.shoppingitemAdapter.notifyDataSetChanged()
+        }
+    }
+
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+
+        // Callback which is used to create the ItemTouch helper. Only enables left swipe.
+        // Use ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) to also enable right swipe.
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            // Enables or Disables the ability to move items up and down.
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            // Callback triggered when a user swiped an item.
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                shoppingItems.removeAt(position)
+                shoppingitemAdapter.notifyDataSetChanged()
+            }
+        }
+        return ItemTouchHelper(callback)
+    }
+
 }
